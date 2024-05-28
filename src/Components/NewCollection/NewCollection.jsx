6e989f36511c7b7115a0700b75531350e4224e3b
Nipun Chaurasia <nc1135@srmist.edu.in>
2024-05-28 21:09:43 +0530

    Updated newcollection component

--- src/Components/NewCollection/NewCollection.jsx
+++ src/Components/NewCollection/NewCollection.jsx
@@ -4,7 +4,7 @@ import new_collection from '../Assets/new_collections'
 import Item from '../Item/Item'
 const NewCollection = () => {
   return (
-    <div className='new-collection'>
+    <div className='new-collections'>
         <h1>NEW COLLECTIONS</h1>
         <hr />
         <div className="collections">
