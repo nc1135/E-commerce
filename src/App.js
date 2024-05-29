589f2e41c2d3aa3fcfaf4bbe6f120d66a6f71ba9
Nipun Chaurasia <nc1135@srmist.edu.in>
2024-05-29 23:48:52 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Product from './Pages/Product';
 import Cart from './Pages/Cart';
 import LoginSignup from './Pages/LoginSignup';
 import Footer from './Components/Footer/Footer';
+import mens_banner from './Components/Assets/banner_mens.png'
+import womens_banner from './Components/Assets/banner_women.png'
+import kids_banner from './Components/Assets/banner_kids.png'
 function App() {
   return (
     <div className="App">
@@ -14,9 +17,9 @@ function App() {
         <Navbar/>
         <Routes>
           <Route path='/' element={<Shop/>}/>
-          <Route path='/mens' element={<ShopCategory category="mens"/>}/>
-          <Route path='/womens' element={<ShopCategory category="womens" />}/>
-          <Route path='/kids' element={<ShopCategory category = "kids" />} />
+          <Route path='/mens' element={<ShopCategory banner= {mens_banner} category="mens"/>}/>
+          <Route path='/womens' element={<ShopCategory banner={womens_banner} category="womens" />}/>
+          <Route path='/kids' element={<ShopCategory banner={kids_banner} category = "kids" />} />
           <Route path='product' element={<Product/>}>
             < Route path=':productId' element={<Product/>}/>
           </Route>
