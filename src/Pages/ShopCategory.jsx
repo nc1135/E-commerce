879601b0753f9b73bc239c5260d5f11b0803925a
Nipun Chaurasia <nc1135@srmist.edu.in>
2024-05-29 22:43:50 +0530

    Updated ShopCategory Page

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-
-const ShopCategory = () => {
+import React, { useContext } from 'react'
+import './CSS/ShopCategory.css'
+import dropdown_icon from '../Components/Assets/dropdown_icon.png'
+import Item from '../Components/Item/Item.jsx'
+const ShopCategory = (props) => {
+  const {all_product}  = useContext()
   return (
-    <div>
-        
+    <div className='shop-category'>
+        <img src={props.banner} alt="" />
+        <div className="shopcategory-indexSort">
+          <p>
+            <span>Showing 1-12</span> out of 36 products
+          </p>
+          <div className="shopcategory-sort">
+            Sort by <img src={dropdown_icon} alt="" />
+          </div>
+        </div>
+        <div className="shopcategory-products">
+          {all_product.map((item, i)=>{
+            if(props.category===item.category){
+              return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+            }
+            
+          })}
+        </div>
     </div>
   )
 }
