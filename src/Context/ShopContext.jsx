9683e64bff55853f632d208eab285d4a24ec34ee
Nipun Chaurasia <nc1135@srmist.edu.in>
2024-05-30 23:39:29 +0530

    Update

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -9,7 +9,7 @@ const ShopContextProvider = (props) => {
     
     return (
         <ShopContext.Provider value={contextValue}>
-            {props.children};
+            {props.childeen};
         </ShopContext.Provider>
   )
 }
