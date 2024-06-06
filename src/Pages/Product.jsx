a8ea50bab522d20a41c6540f071785898407d821
Nipun Chaurasia <nc1135@srmist.edu.in>
2024-06-06 23:33:21 +0530

    Update Product Display

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import { ShopContext } from '../Context/ShopContext'
 import { useParams } from 'react-router-dom'
 import Breadcrum from '../Components/Breadcrums/Breadcrum'
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay'
 
 const Product = () => {
   const{all_product}= useContext(ShopContext)
@@ -9,7 +10,8 @@ const Product = () => {
   const product = all_product.find((e)=>e.id === Number(productId));
   return (
     <div>
-        <Breadcrum product={product}/>    
+        <Breadcrum product={product}/>
+        <ProductDisplay product={product}/>    
     </div>
   )
 }
